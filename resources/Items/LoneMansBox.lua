1ff8e46450b1f8ca61bddee55361b8f1d487f942
WinslowRoR <132460169+WinslowRoR@users.noreply.github.com>
2023-05-04 12:17:54 +0100

    Update and rename ChisseledGranite.lua to LoneMansBox.lua

--- resources/Items/LoneMansBox.lua
+++ resources/Items/LoneMansBox.lua
@@ -2,7 +2,7 @@ local item = Item("Lone Man's Box")
 item.pickupText = "Spawn a Permanent drone upon Leveling up"
 item.sprite = Sprite.load("LoneMansBox", "resources/Items/sprites/Lone Man's Box", 1, 15, 15)
 item:setLog{
-	group = "uncommon",
+	group = "rare",
 	description = "Spawn a Permanent drone upon Leveling up",
 	story = "Now this is interesting, once i felt the pain leave from my body i awoke to a strange place, I have no idea where i was but i'm GLAD i got out of there, it gave me the creeps. But i feel like i'm still there, i hear them taunting me in every corner. I feel like everyone is watching my every move.",
 	priority = "&g&holy shit&!&",
