f27c12e52b7b96ce2d5a21fabe72cefb6f614c36
Winslow <132460169+WinslowRoR@users.noreply.github.com>
2023-05-21 23:52:12 +0200

    Update menuChanger.lua

--- resources/Misc/menuChanger.lua
+++ resources/Misc/menuChanger.lua
@@ -1,21 +1,12 @@
-local title = Sprite.find("sprTitle")
-local groundStripOLD = Sprite.find("GroundStrip")
 local titleBG = Sprite.find("titleScreen")
 local logo = Sprite.find("Logos")
 if modloader.checkMod("starstorm") then
 	callback.register("postLoad", function()
 	local MeridianLogo = Sprite.load("Logo", "resources/Misc/Sprites/logo", 1, 219, 100)
-	local titleMerdStarstorm = Sprite.load("sprTitleMerdStarstorm", "resources/Misc/Sprites/sprTitleStarstormMeridian", 1, 230, 70) 
+	local titleMerdStarstorm = Sprite.load("sprTitleSS1", "resources/Misc/Sprites/sprTitleSS1", 1, 191, 50) 
 	logo:replace(MeridianLogo)
 	title:replace(titleMerdStarstorm)	
-end)
+	end)
 end
-
-local groundStrip = Sprite.load("Strip1", "resources/Misc/Sprites/groundStrip1", 1, 0, 0)
-groundStripOLD:replace(groundStrip)
-	
-local titleMeridian = Sprite.load("Title_Meridian1", "resources/Misc/Sprites/sprTitle1", 1, 230, 75)
-title:replace(titleMeridian)
-
-local titleBGMeridian = Sprite.load("titleScreenMeridian1", "resources/Misc/Sprites/titleScreen1", 1, 0, 0)
-titleBG:replace(titleBGMeridian)
+Sprite.find("sprTitle"):replace(Sprite.load("resources/Misc/Sprites/sprTitle1", 1, 191, 50))
+Sprite.find("titleScreen"):replace(Sprite.load("resources/Misc/Sprites/titleScreen1", 1, 0, 0))
