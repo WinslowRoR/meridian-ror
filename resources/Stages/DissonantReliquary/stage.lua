c6f638131d55d029f745e488f6a8ef329e300da1
Winslow <132460169+WinslowRoR@users.noreply.github.com>
2023-05-22 00:30:20 +0200

    Update stage.lua

--- resources/Stages/DissonantReliquary/stage.lua
+++ resources/Stages/DissonantReliquary/stage.lua
@@ -1756,7 +1756,7 @@ local STAGE_OBJECTS = {
 }
 
 local STAGE_BACKGROUNDS = {
-    [9800] = {"CR_5_1_bg2", "RoRMaps", 1, 0, 0.40, 0.80, 0, 0, 0, 0.60 },
+    [9800] = {"ReliquaryBG2", "RoRMaps", 1, 0, 0.40, 0.80, 0, 0, 0, 0.60 },
     [9900] = {"HugePlanet", "RoRMaps", 0, 0, 1, 1, -631, 0, 0.40, 0 },
     [10000] = {"Reliquarysky", "RoRMaps", 1, 1, 0.90, 0.90, 0, 0, 0, 0 },
 }
