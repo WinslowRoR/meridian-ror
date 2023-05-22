c91bb77bbff726b47fe4235cfc7084b7df5b8d13
Winslow <132460169+WinslowRoR@users.noreply.github.com>
2023-05-22 10:30:17 +0200

    menu changer minor fix

--- resources/Misc/menuChanger.lua
+++ resources/Misc/menuChanger.lua
@@ -2,10 +2,8 @@ local titleBG = Sprite.find("titleScreen")
 local logo = Sprite.find("Logos")
 if modloader.checkMod("starstorm") then
 	callback.register("postLoad", function()
-	local MeridianLogo = Sprite.load("Logo", "resources/Misc/Sprites/logo", 1, 219, 100)
-	local titleMerdStarstorm = Sprite.load("sprTitleSS1", "resources/Misc/Sprites/sprTitleSS1", 1, 191, 50) 
-	logo:replace(MeridianLogo)
-	title:replace(titleMerdStarstorm)	
+	Sprite.find("Logos"):replace(Sprite.load("resources/Misc/Sprites/logo", 1, 219, 100))
+	Sprite.find("sprTitle"):replace(Sprite.load("resources/Misc/Sprites/sprTitleSS1", 1, 191, 50))
 	end)
 end
 Sprite.find("sprTitle"):replace(Sprite.load("resources/Misc/Sprites/sprTitle1", 1, 191, 50))
